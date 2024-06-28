e5789c473946705ade5c79437d1d5067cea73edd
Rafael <rafael.sousadrd@gmail.com>
2024-06-28 00:18:17 -0300

    criando o setup do projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+const dateEvent = new Date("Mar 29, 2025 00:00:00");
+const timeStampEvent = dateEvent.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampEvent - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 *60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutosEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+    
+    if(distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Já aniversariei esse ano :)';
+    }
+}, 1000)
